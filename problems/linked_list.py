c6574f106352ff971c5e5d730450e1d9952a32fb
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2025-11-19 15:04:41 +0400

    move the tests to linkedlist

--- problems/linked_list.py
+++ problems/linked_list.py
@@ -64,3 +64,52 @@ class LinkedList:
         while cur:
             yield cur.value
             cur = cur.next
+
+def test_find_middle_empty() -> None:
+    ll: LinkedList = LinkedList()
+    assert ll.find_middle() is None
+
+def test_insert_head_and_to_list() -> None:
+    ll: LinkedList = LinkedList()
+    ll.insert_head(3)
+    ll.insert_head(2)
+    ll.insert_head(1)
+    assert ll.to_list() == [1, 2, 3]
+
+def test_append() -> None:
+    ll: LinkedList = LinkedList()
+    ll.append(1)
+    ll.append(2)
+    ll.append(3)
+    assert ll.to_list() == [1, 2, 3]
+
+def test_delete_head_and_middle() -> None:
+    ll: LinkedList = LinkedList()
+    for v in [1, 2, 3, 4]:
+        ll.append(v)
+    assert ll.delete(1) is True
+    assert ll.to_list() == [2, 3, 4]
+    assert ll.delete(3) is True
+    assert ll.to_list() == [2, 4]
+    assert ll.delete(42) is False
+
+def test_reverse_cases() -> None:
+    ll: LinkedList = LinkedList()
+    assert ll.to_list() == []
+    ll.reverse()  # reverse empty
+    ll.append(1)
+    ll.reverse()  # reverse single
+    assert ll.to_list() == [1]
+    for v in [2, 3, 4]:
+        ll.append(v)
+    ll.reverse()
+    assert ll.to_list() == [4, 3, 2, 1]
+
+def test_find_middle_even_and_odd() -> None:
+    ll: LinkedList = LinkedList()
+    for v in [10, 20, 30, 40, 50]:
+        ll.append(v)
+    assert ll.find_middle() == 30  # odd length
+    ll.append(60)
+    # even length -> left middle (index len//2 - 1) -> index 2 -> 30
+    assert ll.find_middle() == 30
