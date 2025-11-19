5b8e3878ade555bd3319a09243de7b0114bae573
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2025-11-19 14:40:11 +0400

    move the tests close to the code

--- problems/binary_search_tree.py
+++ problems/binary_search_tree.py
@@ -59,3 +59,39 @@ class BinarySearchTree:
 
     def delete(self: 'BinarySearchTree', value: int) -> bool:  # type: ignore
         pass  # TODO: implement
+
+
+def test_insert_and_inorder_sorted() -> None:
+    bst = BinarySearchTree()
+    for v in [7, 3, 9, 1, 5, 8, 10]:
+        bst.insert(v)
+    assert bst.inorder() == [1, 3, 5, 7, 8, 9, 10]
+
+def test_search_existing_and_missing() -> None:
+    bst = BinarySearchTree()
+    for v in [4, 2, 6]:
+        bst.insert(v)
+    assert bst.search(2) is True
+    assert bst.search(5) is False
+
+def test_height_and_balance() -> None:
+    bst = BinarySearchTree()
+    for v in [5, 3, 8, 2, 4, 7, 9]:
+        bst.insert(v)
+    assert bst.height() == 3
+    assert bst.is_balanced() is True
+    bst.insert(1)
+    bst.insert(0)
+    assert bst.is_balanced() is False
+
+def test_find_min_max() -> None:
+    bst = BinarySearchTree()
+    for v in [5, 3, 8, 2, 4, 7, 9]:
+        bst.insert(v)
+    assert bst.find_min() == 2
+    assert bst.find_max() == 9
+
+test_insert_and_inorder_sorted()
+test_search_existing_and_missing()
+test_height_and_balance()
+test_find_min_max()
